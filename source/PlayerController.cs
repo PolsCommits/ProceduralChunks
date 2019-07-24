416730bb254b23b6456f7bc6b63a3693ffe0ecda
PolsCommits <paul.rauca@outlook.com>
2019-07-24 17:59:55 +0300

    ADD first build ADD source files

---
+++ source/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5; // units per second
+    public float turnSpeed = 90; // degrees per second
+    public float jumpSpeed = 8;
+    public float gravity = 9.8f;
+    public float vSpeed = 0; // current vertical velocity
+    public float interactRange = 0f;
+
+    public GameObject target;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void ToggleCursorLockState()
+    {
+        if (Cursor.lockState != CursorLockMode.None)
+            Cursor.lockState = CursorLockMode.None;
+        else
+            Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void Update()
+    {
+        Vector3 vel = transform.forward * Input.GetAxis("Vertical") * speed + transform.right * Input.GetAxis("Horizontal") * speed;
+        var controller = GetComponent<CharacterController>();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ToggleCursorLockState();
+
+        if (controller.isGrounded)
+        {
+            vSpeed = 0; // grounded character has vSpeed = 0...
+            if (Input.GetKeyDown("space"))
+            { // unless it jumps:
+                vSpeed = jumpSpeed;
+            }
+        }
+
+        Camera.main.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+
+        // apply gravity acceleration to vertical speed:
+        vSpeed -= gravity * Time.deltaTime;
+        vel.y = vSpeed; // include vertical speed in vel
+                        // convert vel to displacement and Move the character:
+        controller.Move(vel * Time.deltaTime);
+
+        //Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.position + Camera.main.transform.forward * interactRange, Color.red);
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward, Color.green);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, interactRange))
+        {
+            target = hit.collider.gameObject;
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(target.GetComponent<VoxelChunk>())
+                {
+                    target.GetComponent<VoxelChunk>().RemoveVoxel(target.GetComponent<VoxelChunk>().GetRemoveAtPosition(hit.point, Camera.main.transform.forward, hit));
+                }
+            }
+            else if(Input.GetMouseButtonDown(1))
+            {
+                if (target.GetComponent<VoxelChunk>())
+                {
+                    target.GetComponent<VoxelChunk>().AddVoxel(target.GetComponent<VoxelChunk>().GetAddAtPosition(hit.point, Camera.main.transform.forward, hit));
+                }
+            }
+        }
+        else
+        {
+            target = null;
+        }
+    }
+}
---
+++ source/ProceduralVoxelMap.cs
@@ -0,0 +1,257 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralVoxelMap : MonoBehaviour
+{
+    // List of generated chunks
+    List<GameObject> chunks = new List<GameObject>();
+    // Reference to closest chunk
+    public GameObject currentChunk;
+    // Player reference
+    public PlayerController player;
+    // Number of chunks to render around player
+    public int renderDistance = 3;
+    // Size of spawn area
+    public int spawnAreaSize = 3;
+    // Chunk variables
+    public int chunkWidth, chunkHeight, chunkDepth;
+    // Default mesh material
+    public Material defMaterial;
+    // World seed
+    public int seed;
+    // World offsets
+    private int offX, offY;
+
+    private void Start()
+    {
+        for(int i = 0; i < spawnAreaSize; i++)
+        {
+            for(int j = 0; j < spawnAreaSize; j++)
+            {
+                chunks.Add(NewChunk(i, j, defMaterial));
+            }
+        }
+
+        Random.InitState(seed);
+        offX = Random.Range(0, 1000000);
+        offY = Random.Range(0, 1000000);
+    }
+
+    // Returns an instantiated chunk
+    private GameObject NewChunk(int posX, int posY, Material defaultMaterial)
+    {
+        GameObject chunk = new GameObject();
+        chunk.name = "Chunk " + posX + " " + posY;
+        chunk.transform.position = new Vector3(chunkWidth * posX, 0, chunkDepth * posY);
+        chunk.transform.parent = transform;
+        chunk.tag = "Chunk";
+
+        Debug.Log(transform.position);
+
+        chunk.AddComponent<VoxelChunk>();
+        chunk.GetComponent<VoxelChunk>().chunkX = posX;
+        chunk.GetComponent<VoxelChunk>().chunkY = posY;
+        chunk.GetComponent<VoxelChunk>().width = chunkWidth;
+        chunk.GetComponent<VoxelChunk>().height = chunkHeight;
+        chunk.GetComponent<VoxelChunk>().depth = chunkDepth;
+        chunk.GetComponent<VoxelChunk>().defaultMaterial = defMaterial;
+
+        chunk.GetComponent<VoxelChunk>().SetupChunk(0, 0);
+        chunk.GetComponent<VoxelChunk>().GenerateMesh();
+
+        UpdateNeighbours(chunk.GetComponent<VoxelChunk>());
+
+        return chunk;
+    }
+
+    private void Update()
+    {
+        FindClosestChunk();
+
+        AddNewChunks();
+    }
+
+    // Check if new chuynks need to be generated
+    private void AddNewChunks()
+    {
+        int x = currentChunk.GetComponent<VoxelChunk>().chunkX;
+        int y = currentChunk.GetComponent<VoxelChunk>().chunkY;
+
+        for(int i = x - renderDistance; i < x + renderDistance; i++)
+        {
+            for(int j = y - renderDistance; j < y + renderDistance; j++)
+            {
+                bool exists = false;
+                foreach(GameObject chunkObj in chunks)
+                {
+                    if(i == chunkObj.GetComponent<VoxelChunk>().chunkX && j == chunkObj.GetComponent<VoxelChunk>().chunkY)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if(!exists)
+                {
+                    GameObject newChunk = NewChunk(i, j, defMaterial);
+                    chunks.Add(newChunk);
+                }
+
+            }
+        }
+    }
+
+    private void FindClosestChunk()
+    {
+        float dist = 999999f;
+        GameObject auxChunk = null;
+
+        foreach(GameObject chunkObj in chunks)
+        {
+            if (Vector3.Distance(player.transform.position, chunkObj.transform.position) < dist)
+            {
+                dist = Vector3.Distance(player.transform.position, chunkObj.transform.position);
+                auxChunk = chunkObj;
+            }
+        }
+
+        currentChunk = auxChunk;
+    }
+
+    private void UpdateNeighbours(VoxelChunk targetChunk)
+    {
+        //Update neighbours for single chunk
+        foreach (GameObject chunkObj in chunks)
+            {
+                VoxelChunk chunk = chunkObj.GetComponent<VoxelChunk>();
+
+                if (chunk != targetChunk)
+                {
+                    if (adjCheck(chunk, targetChunk))
+                    {
+                        setAdjChunk(targetChunk, chunk);
+                    }
+                }
+            }
+    }
+
+    private bool adjCheck(VoxelChunk a, VoxelChunk b)
+    {
+        if (a != b)
+        {
+            if (a.chunkX >= b.chunkX - 1 && a.chunkX <= b.chunkX + 1)
+            {
+                if (a.chunkY >= b.chunkY - 1 && a.chunkY <= b.chunkY + 1)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void setAdjChunk(VoxelChunk source, VoxelChunk target)
+    {
+        int x = target.chunkX - source.chunkX;
+        int y = target.chunkY - source.chunkY;
+
+        if (x == -1)
+        {
+            if (y == -1)
+            {
+                source.adjChunks[0] = target;
+                target.adjChunks[4] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+                return;
+            }
+            else if (y == 0)
+            {
+                source.adjChunks[1] = target;
+                target.adjChunks[5] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+
+                return;
+            }
+            else if (y == 1)
+            {
+                source.adjChunks[2] = target;
+                target.adjChunks[6] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+
+                return;
+            }
+        }
+        else if (x == 0)
+        {
+            if (y == -1)
+            {
+                source.adjChunks[7] = target;
+                target.adjChunks[3] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+
+                return;
+            }
+            else if (y == 1)
+            {
+                source.adjChunks[3] = target;
+                target.adjChunks[7] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+
+                return;
+            }
+        }
+        else if (x == 1)
+        {
+            if (y == -1)
+            {
+                source.adjChunks[6] = target;
+                target.adjChunks[2] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+
+                return;
+            }
+            else if (y == 0)
+            {
+                source.adjChunks[5] = target;
+                target.adjChunks[1] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+
+                return;
+            }
+            else if (y == 1)
+            {
+                source.adjChunks[4] = target;
+                target.adjChunks[0] = source;
+
+                source.pendingUpdate = true;
+                target.pendingUpdate = true;
+
+
+                return;
+            }
+        }
+    }
+
+}
